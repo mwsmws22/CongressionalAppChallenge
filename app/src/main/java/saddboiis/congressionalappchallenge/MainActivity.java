9d50124339a6dce1ad5641236de49354cf9f9ab9
mwsmws22 <mitchellwsmith@hotmail.com>
2016-10-14 00:35:36 -0700

    added comment

--- app/src/main/java/saddboiis/congressionalappchallenge/MainActivity.java
+++ app/src/main/java/saddboiis/congressionalappchallenge/MainActivity.java
@@ -9,5 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    } //hey yo yo yo testo
+    } //hey yo yo yo testo branch
 }
